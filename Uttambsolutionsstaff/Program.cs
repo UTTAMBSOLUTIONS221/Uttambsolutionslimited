df54b96ff1db3c2e632696dd68228935354581fe
FRANCIS KINGORI <francisnguru448@gmail.com>
2024-11-12 16:07:20 +0300

    update

--- Uttambsolutionsstaff/Program.cs
+++ Uttambsolutionsstaff/Program.cs
@@ -6,6 +6,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.Addcustomjwtauthentication();
+//Database configuration 
+//var Dbhost = Environment.GetEnvironmentVariable("DB_HOST");
+//var Dbname = Environment.GetEnvironmentVariable("DB_NAME");
+//var Dbusername = Environment.GetEnvironmentVariable("DB_USERNAME");
+//var Dbpassword = Environment.GetEnvironmentVariable("DB_SA_PASSWORD");
+//var Connectionstring = $"Data Source={Dbhost};Database={Dbname};User Id={Dbusername};Password={Dbusername};TrustServerCertificate=true";
+//builder.Services.AddDbContext<Staff>
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
