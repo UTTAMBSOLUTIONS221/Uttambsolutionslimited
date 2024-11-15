e0d0ba82564a47d698b762aebeca2b91e6e2f8ec
FRANCIS KINGORI <francisnguru448@gmail.com>
2024-11-15 18:48:36 +0300

    update

--- ApiGateWay/Program.cs
+++ ApiGateWay/Program.cs
@@ -1,10 +1,30 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 var builder = WebApplication.CreateBuilder(args);
+
+// Add CORS services
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000") // Replace with your frontend's origin
+              .AllowAnyHeader()                    // Allow all headers
+              .AllowAnyMethod()                   // Allow all HTTP methods
+              .AllowCredentials();               // Optional                 
+    });
+});
+
 builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
     .AddJsonFile("ocelot.json", optional: true, reloadOnChange: true)
     .AddEnvironmentVariables();
 builder.Services.AddOcelot(builder.Configuration);
 var app = builder.Build();
-await app.UseOcelot();
+
+// Use Ocelot
+app.UseRouting();
+
+// Enable the CORS policy
+app.UseCors("AllowSpecificOrigin");
+app.UseOcelot().Wait();
+
 app.Run();
--- Uttambsolutionsstaff/Program.cs
+++ Uttambsolutionsstaff/Program.cs
@@ -2,17 +2,6 @@ using Jwtauthenticationmanager;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add CORS services
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowReactApp", policy =>
-    {
-        policy.WithOrigins("http://localhost:3000") // Replace with your frontend's origin
-              .AllowAnyHeader()                     // Allow all headers
-              .AllowAnyMethod();                    // Allow all HTTP methods
-    });
-});
-
 builder.Services.AddControllers();
 builder.Services.Addcustomjwtauthentication();
 builder.Services.AddEndpointsApiExplorer();
@@ -23,9 +12,6 @@ var app = builder.Build();
 // Enable Swagger
 app.UseSwagger();
 app.UseSwaggerUI();
-
-// Enable the CORS policy
-app.UseCors("AllowReactApp");
 
 app.UseAuthorization();
 app.MapControllers();
